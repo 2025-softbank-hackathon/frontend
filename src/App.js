e39bb6d6f82c8a4cc81df998a0afbcb5c0eefca0
Leeyoujin00 <cymn6032@gmail.com>
2025-11-07 15:27:29 +0900

    chore: 파일 삭제

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import ChatRoom from './components/ChatRoom';
-import './styles/globals.css';
-import './styles/chat.css';
-
-function App() {
-  return <ChatRoom />;
-}
-
-export default App;
--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders nickname modal on initial load', () => {
-  render(<App />);
-  expect(screen.getByRole('heading', { name: '닉네임을 입력해주세요' })).toBeInTheDocument();
-});
